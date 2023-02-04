f4fc8897a31d6b7a9248714daf7c37dc386de9f2
janikF1 <jolograggy45@gmail.com>
2023-02-04 14:29:19 +0100

    initiali project structure

--- main.c
+++ main.c
@@ -7,11 +7,12 @@ void start_game()
 {
     char selected_word[10];
     char guessed_word[sizeof(selected_word)];
-    char *ptr_to_selected_word = &selected_word;
-    char *ptr_to_guessed_word = &guessed_word;
+    char *ptr_to_selected_word = selected_word;
+    char *ptr_to_guessed_word = guessed_word;
     init_guessed_word(ptr_to_guessed_word);
 }
 
 void main()
 {
+    start_game();
 }
--- main.h
+++ main.h
@@ -2,3 +2,4 @@
 #include <stdio.h>
 
 void main(void);
+void start_game(void);
